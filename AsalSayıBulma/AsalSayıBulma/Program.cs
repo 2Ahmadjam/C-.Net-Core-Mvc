222edc6a62f0d8868d5c3cf04295179a9a9a6a4f
AHMAD BADR EDDIN JAMOUS <98274758+2Ahmadjam@users.noreply.github.com>
2022-03-26 02:25:39 +0300

    Add files via upload

---
+++ AsalSayıBulma/AsalSayıBulma/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AsalSayıBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------- 1_10000 Arasında Asal Sayılar ------------------------------- ");
+            int kontrol = 0;
+            int sayac = 0;
+            for (int i = 2; i <= 10000; i++)
+            {
+                kontrol = 0;
+
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        kontrol++;
+                        break;
+                    }
+                }
+                if (kontrol == 0)
+                {
+                    sayac++;
+                    Console.Write(i + "-");
+                }
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Adet :"+ sayac);
+            Console.ReadKey();
+        }
+    }
+}
